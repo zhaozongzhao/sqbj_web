0d4d2143dce3ff793a3b56c3325e5d575b2b75aa
zzz <2206321864@qq.com>
2019-08-06 00:42:40 +0800

    自动选择各种文件的地址修改日志配置文件

--- pyhon_zh_web/mian.py
+++ pyhon_zh_web/mian.py
@@ -1,17 +1,21 @@
+import unittest
+import time
+from Util.var import report_path,Testcase_path
+
+# import pytest
 from HTMLTestRunner import HTMLTestRunner
-import unittest,time
-import pytest
 
 if __name__ == '__main__':
-     now=time.strftime('%Y-%m-%d %H-%m-%M')
-     filename='/Users/hnbl009/gitfile/webtest/pyhon_zh_web/HtmlRoport/'+now+'resul.html'
-     fp = open(filename,'wb')
-     runner = HTMLTestRunner(
-         stream=fp,
-         title='自动化测试报告',
-         description='环境: mac 浏览器 ：chome '
-     )
+    now = time.strftime('%Y-%m-%d %H-%m-%M')
+    filename = report_path + now + 'resul.html'
+    fp = open(filename, 'wb')
+    runner = HTMLTestRunner(
+        stream=fp,
+        title='自动化测试报告',
+        description='环境: mac 浏览器 ：chome '
+    )
 
-     discover = unittest.defaultTestLoader.discover('/Users/hnbl009/gitfile/webtest/pyhon_zh_web/testcase/',pattern = 'test*.py')
-     runner.run(discover)
-     fp.close()
+    discover = unittest.defaultTestLoader.discover(Testcase_path,
+                                                   pattern='test*.py')
+    runner.run(discover)
+    fp.close()
--- pyhon_zh_web/testcase/test_basic_public.py
+++ pyhon_zh_web/testcase/test_basic_public.py
@@ -1,4 +1,4 @@
-import unittest,time
+import unittest, time
 from Action.basic_announcementlist import *
 from Action.page_home import *
 from Action.login import *
@@ -6,8 +6,8 @@ from Util.Excel import *
 from Util.var import *
 from Util.special import *
 
-class test_basic(unittest.TestCase):
 
+class test_basic(unittest.TestCase):
     def successful(self):
         Ba = Basic(self.driver)
         time.sleep(3)
@@ -24,92 +24,92 @@ class test_basic(unittest.TestCase):
         self.driver.get('http://smart.sit2.sqbj.com/portal/login')
         self.pe = parseExcel(Excelobject_path)
         # self.alert = special(self.driver)
-        login(self.driver,'18301565568','111111')
+        login(self.driver, '18301565568', '111111')
         self.Ba = self.successful()
 
-
-
-
-    #创建公示(资质公示)
+    # 创建公示(资质公示)
     def test_creadepublic1(self):
         # self.driver.save_screenshot('/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png')
         coverpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png'
         contentpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/ceshi.png'
-        self.Ba.create_public('自动创建资质公示1','赵宗召','资质公示','自动创建的内容',coverpicture,contentpicture,'新建资质公示')
-        assert '自动创建资质公示1' in self.Ba.select_table_publicelement(1,1,1).text
+        self.Ba.create_public('自动创建资质公示1', '赵宗召', '资质公示', '自动创建的内容', coverpicture, contentpicture, '新建资质公示')
+        assert '自动创建资质公示1' in self.Ba.select_table_publicelement(1, 1, 1).text
 
-    #创建公示(物业服务)
+    # 创建公示(物业服务)
     def test_creadepublic2(self):
         # self.driver.save_screenshot('/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png')
         coverpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png'
         contentpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/ceshi.png'
-        self.Ba.create_public('自动创建物业服务1','赵宗召','物业服务','自动创建的内容',coverpicture,contentpicture,'物业服务')
-        assert '自动创建物业服务1' in self.Ba.select_table_publicelement(1,1,1).text
+        self.Ba.create_public('自动创建物业服务1', '赵宗召', '物业服务', '自动创建的内容', coverpicture, contentpicture, '物业服务')
+        assert '自动创建物业服务1' in self.Ba.select_table_publicelement(1, 1, 1).text
 
-    #创建公示(收费标准)
+    # 创建公示(收费标准)
     def test_creadepublic3(self):
         # self.driver.save_screenshot('/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png')
         coverpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png'
         contentpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/ceshi.png'
-        self.Ba.create_public('自动创建收费标准1','赵宗召','收费标准','自动创建的内容',coverpicture,contentpicture,'收费标准')
-        assert '自动创建收费标准1' in self.Ba.select_table_publicelement(1,1,1).text
+        self.Ba.create_public('自动创建收费标准1', '赵宗召', '收费标准', '自动创建的内容', coverpicture, contentpicture, '收费标准')
+        assert '自动创建收费标准1' in self.Ba.select_table_publicelement(1, 1, 1).text
 
-    #创建公示(设置运行公示)
+    # 创建公示(设置运行公示)
     def test_creadepublic4(self):
         # self.driver.save_screenshot('/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png')
         coverpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png'
         contentpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/ceshi.png'
-        self.Ba.create_public('自动创建设置运行公示1','赵宗召','设备运行公示','自动创建的内容',coverpicture,contentpicture,'设置运行公示')
-        assert '自动创建设置运行公示1' in self.Ba.select_table_publicelement(1,1,1).text
+        self.Ba.create_public('自动创建设置运行公示1', '赵宗召', '设备运行公示', '自动创建的内容', coverpicture, contentpicture, '设置运行公示')
+        assert '自动创建设置运行公示1' in self.Ba.select_table_publicelement(1, 1, 1).text
 
-
-    #发布服务公示
+    # 发布服务公示
     def test_release_public1(self):
         coverpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png'
         contentpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/ceshi.png'
-        self.Ba.release_public('发布公示1','赵宗召','设备运行公示','自动创建的内容',coverpicture,contentpicture)
-        assert '发布公示1' in self.Ba.select_table_publicelement(1,1,1).text
+        self.Ba.release_public('发布公示1', '赵宗召', '设备运行公示', '自动创建的内容', coverpicture, contentpicture)
+        assert '发布公示1' in self.Ba.select_table_publicelement(1, 1, 1).text
 
-     #发布服务公示
+        # 发布服务公示
+
     def test_release_public2(self):
         coverpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png'
         contentpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/ceshi.png'
-        self.Ba.release_public('发布资质公示1','赵宗召','资质公示','自动创建的内容',coverpicture,contentpicture)
-        assert '发布资质公示1' in self.Ba.select_table_publicelement(1,1,1).text
+        self.Ba.release_public('发布资质公示1', '赵宗召', '资质公示', '自动创建的内容', coverpicture, contentpicture)
+        assert '发布资质公示1' in self.Ba.select_table_publicelement(1, 1, 1).text
 
-     #发布服务公示
+        # 发布服务公示
+
     def test_release_public3(self):
         coverpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png'
         contentpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/ceshi.png'
-        self.Ba.release_public('发布物业服务1','赵宗召','物业服务','自动创建的内容',coverpicture,contentpicture)
-        assert '发布物业服务1' in self.Ba.select_table_publicelement(1,1,1).text
+        self.Ba.release_public('发布物业服务1', '赵宗召', '物业服务', '自动创建的内容', coverpicture, contentpicture)
+        assert '发布物业服务1' in self.Ba.select_table_publicelement(1, 1, 1).text
 
-     #发布服务公示
+        # 发布服务公示
+
     def test_release_public4(self):
         coverpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/1.png'
         contentpicture = '/Users/hnbl009/gitfile/webtest/pyhon_zh_web/logs/ceshi.png'
-        self.Ba.release_public('发布收费标准1','赵宗召','收费标准','自动创建的内容',coverpicture,contentpicture)
-        assert '发布收费标准1' in self.Ba.select_table_publicelement(1,1,1).text
+        self.Ba.release_public('发布收费标准1', '赵宗召', '收费标准', '自动创建的内容', coverpicture, contentpicture)
+        assert '发布收费标准1' in self.Ba.select_table_publicelement(1, 1, 1).text
 
-
-    #删除服务公示
+    # 删除服务公示
     def test_delete_public1(self):
         # Ba = self.successful()
         self.Ba.Enter_the_public()
         time.sleep(5)
-        element = self.Ba.select_table_publicelement(1,1,1).text
-        self.Ba.click_delete_button(1,6,'取消')
-        assert element in self.Ba.select_table_publicelement(1,1,1).text
+        element = self.Ba.select_table_publicelement(1, 1, 1).text
+        self.Ba.click_delete_button(1, 6, '取消')
+        assert element in self.Ba.select_table_publicelement(1, 1, 1).text
 
-     #删除服务公示
+        # 删除服务公示
+
     def test_delete_public2(self):
         # Ba = self.successful()
         self.Ba.Enter_the_public()
-        element = self.Ba.select_table_publicelement(1,1,1).text
-        self.Ba.click_delete_button(1,6,'确定')
-        assert element not in self.Ba.select_table_publicelement(1,1,1).text
+        element = self.Ba.select_table_publicelement(1, 1, 1).text
+        self.Ba.click_delete_button(1, 6, '确定')
+        assert element not in self.Ba.select_table_publicelement(1, 1, 1).text
 
-     #服务公示标签筛选
+        # 服务公示标签筛选
+
     def test_choose_public(self):
         self.Ba.Enter_the_public()
         self.Ba.choose_lable_screening('物业服务')
@@ -118,9 +118,7 @@ class test_basic(unittest.TestCase):
         time.sleep(5)
         print(self.Ba.getRownumberCount())
         for i in range(self.Ba.getRownumberCount()):
-            assert '物业服务'  == self.Ba.select_table_publicelement(i,2,1).text
-
-
+            assert '物业服务' == self.Ba.select_table_publicelement(i, 2, 1).text
 
     def tearDown(self):
         info('##############################服务公示测试结束#######################################')
